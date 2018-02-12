e4ea89ba7495a402c954ff5838c1619f0d9dc6be
tribhuvan <tribhuvandatta@gmail.com>
2018-02-12 15:20:44 -0500

    PlayingCardsTest added.

---
+++ PlayingCardsTest/CardsTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlayingCards;
+
+namespace PlayingCardsTest
+{
+    public class CardsTest //Card shuffling and dealing
+    {
+        //excute application
+        static void Main(string[] args)
+        {
+            try //exception handling
+            {
+                DeckOfCards myDeckOfCards = new DeckOfCards();
+                myDeckOfCards.Shuffle(); //place cards in random order
+
+                //print all 52 cards in the order in which they are dealt
+                for (int i = 1; i <= 52; i++)
+                {
+                    Console.Write("{0,-25}", myDeckOfCards.dealCard()); //deal and display a card
+                    if (i % 3 == 0) //output a new line after every third card
+                    {
+                        Console.WriteLine();
+                    }
+                } //end for
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }//end main
+    }//end class
+}//end namespace
